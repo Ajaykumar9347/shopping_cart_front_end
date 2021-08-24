8ef49da69772de84d76fdaf203a803972e8f8477
Ajay Kumar <neseajaykumar9347@gmail.com>
2021-08-24 16:21:52 +0530

    ecommerce

---
+++ src/core/Cart.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+import Base from "./Base";
+import Card from "./Card";
+import { loadCart } from "./helper/cartHelper";
+import StripeCheckout from "./StripeCheckout";
+import Payment from "./Payment";
+
+const Cart = () => {
+  const [products, setProducts] = useState([]);
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    setProducts(loadCart());
+  }, [reload]);
+  const loadAllProducts = (products) => {
+    return (
+      <div>
+        <h2>Your cart</h2>
+        {products.map((product, index) => {
+          return (
+            <Card
+              key={index}
+              product={product}
+              addtoCart={false}
+              removeFromCart={true}
+              setReload={setReload}
+              reload={reload}
+            />
+          );
+        })}
+      </div>
+    );
+  };
+
+  const loadCheckout = () => {
+    return (
+      <div>
+        <StripeCheckout
+          products={products}
+          setReload={setReload}
+          reload={reload}
+        />
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Cart" description=" Get ready to checkout right from here :)">
+      <div className="row text-center">
+        <div className="col-6">
+          {products.length > 0 ? (
+            loadAllProducts(products)
+          ) : (
+            <h3>No products in the cart</h3>
+          )}
+        </div>
+        <div className="col-6">
+          {loadCheckout()}
+          <Payment products={products} setReload={setReload} reload={reload} />
+          {/* <p>Braintree</p> */}
+        </div>
+      </div>
+    </Base>
+  );
+};
+
+export default Cart;
---
+++ src/core/StripeCheckout.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../auth/helper";
+import { cartEmpty } from "./helper/cartHelper";
+import Stripecheckout from "react-stripe-checkout";
+import { API } from "../backend";
+
+const StripeCheckout = ({
+  products,
+  setReload = (f) => f,
+  reload = undefined,
+}) => {
+  const [data, setData] = useState({
+    loading: false,
+    success: false,
+    error: "",
+    address: "",
+  });
+
+  const token = isAuthenticated() && isAuthenticated().token;
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+
+  const getFinalAmount = () => {
+    let amount = 0;
+    products.map((product) => {
+      amount = amount + product.price;
+    });
+    return amount;
+  };
+
+  const makePayment = (token) => {
+    const body = {
+      token,
+      products,
+    };
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    return fetch(`${API}/stripepayment`, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        //  console.log(response);
+        // call furthur
+        const { status } = response;
+        console.log("STATUS", status);
+        cartEmpty(() => {
+          console.log("Emptied the cart..!");
+          setReload(!reload);
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const showStripeButton = () => {
+    return isAuthenticated() ? (
+      <Stripecheckout
+        stripeKey="pk_test_51I1uftKu3498s3qiCYmr5xPA6icms8MSTVE5CwbfhjFBLZ43H2afTzy47kNgGuywEfxSXXQuibQY9rAnNyPMLyuG0026VdjTvu"
+        token={makePayment}
+        amount={getFinalAmount() * 100}
+        name="Pay with stripe"
+        shippingAddress
+        billingAddress
+      >
+        <button className="btn btn-success">Pay with Stripe</button>
+      </Stripecheckout>
+    ) : (
+      <Link to="/signin">
+        <button className="btn btn-info">Signin</button>
+      </Link>
+    );
+  };
+
+  return (
+    <div>
+      <h3 className="text-white">
+        Stripe Checkout
+        {/* {getFinalAmount()} */}
+      </h3>
+      {showStripeButton()}
+    </div>
+  );
+};
+
+export default StripeCheckout;
